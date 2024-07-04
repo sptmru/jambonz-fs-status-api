2cb1f6ce46a0e9ec964016878952c84eb58fac73
Soslan Aldatov <soslanaldatov@gmail.com>
2024-07-04 17:38:29 +0400

    start API server on app start

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import { Api } from './infrastructure/api/server';
+import { HealthRoute } from './routes/health/health.route';
+
+const api = new Api({
+  plugins: [],
+  routes: [HealthRoute],
+  definitions: [],
+});
+api.listen();
