ef2fa221ef8533a275dd4eb3d356ad7400f2e5d6
Soslan Aldatov <soslanaldatov@gmail.com>
2024-07-04 20:00:02 +0400

    enable new instance calls API routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import { Api } from './infrastructure/api/server';
 import { HealthRoute } from './routes/health/health.route';
+import { InstanceRoute } from './routes/instanceCalls/instance.route';
+import { InstanceCallsDataDefinition } from './domain/definitions/InstanceCallsData.definition';
+import { UpdateInstanceCallsDataDefinition } from './domain/definitions/UpdateInstanceCallsData.definition';
+import { RedisClient } from './infrastructure/redis/client';
 
 const api = new Api({
   plugins: [],
-  routes: [HealthRoute],
-  definitions: [],
+  routes: [HealthRoute, InstanceRoute],
+  definitions: [InstanceCallsDataDefinition, UpdateInstanceCallsDataDefinition],
 });
 api.listen();
+
+RedisClient.getInstance();
