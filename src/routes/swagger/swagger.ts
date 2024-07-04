89f93787b591325bc4737b29be41dd2cbe93c3d1
Soslan Aldatov <soslanaldatov@gmail.com>
2024-07-04 17:31:31 +0400

    add API server

---
+++ src/routes/swagger/swagger.ts
@@ -0,0 +1,34 @@
+import { config } from '../../infrastructure/config/config';
+
+export const swaggerOptions = {
+  openapi: {
+    openapi: '3.0.0',
+    info: {
+      title: 'IVR API',
+      description: 'IVR API Documentation',
+      version: '1.0.0',
+    },
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    servers: [
+      {
+        url: config.api.hostname,
+      },
+    ],
+    components: {
+      securitySchemes: {
+        ApiKeyAuth: {
+          type: 'apiKey',
+          name: 'Authorization',
+          in: 'header',
+        },
+      },
+    },
+    tags: [
+      {
+        name: 'monitoring',
+        description: 'Monitoring endpoints',
+      },
+    ],
+  },
+};
