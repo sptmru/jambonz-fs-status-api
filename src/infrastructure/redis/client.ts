872ae0fbf33c849fb6c220f77c6251787f6bf1b3
Soslan Aldatov <soslanaldatov@gmail.com>
2024-07-04 19:58:14 +0400

    add Redis client

---
+++ src/infrastructure/redis/client.ts
@@ -0,0 +1,34 @@
+import { RedisFunctions, RedisModules, RedisScripts, createClient, RedisClientType } from 'redis';
+import { logger } from '../../misc/Logger';
+import { config } from '../config/config';
+
+export class RedisClient {
+  public static instance: RedisClient | null = null;
+  public client: RedisClientType<RedisModules, RedisFunctions, RedisScripts>;
+
+  private constructor() {
+    this.client = createClient({ url: config.redis.uri });
+
+    this.client.on('connect', () => {
+      logger.info(`Redis connection (re)established`);
+    });
+
+    this.client.on('error', error => {
+      logger.error(`Redis connection error: ${error}`);
+    });
+
+    this.client.on('end', () => {
+      logger.info(`Redis connection closed`);
+    });
+
+    void this.client.connect();
+  }
+
+  public static getInstance(): RedisClient {
+    if (this.instance === null) {
+      this.instance = new RedisClient();
+    }
+
+    return this.instance;
+  }
+}
