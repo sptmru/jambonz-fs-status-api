42fa550e0aeaa0c02a56ef33769403c44c800030
Soslan Aldatov <soslanaldatov@gmail.com>
2024-07-11 02:21:42 +0400

    add logging to InstanceCallsService

--- src/services/instance/InstanceCallsService.ts
+++ src/services/instance/InstanceCallsService.ts
@@ -1,11 +1,13 @@
 import { RedisClient } from '../../infrastructure/redis/client';
 import { config } from '../../infrastructure/config/config';
 import { InstanceCallsData } from '../../domain/types/instance-calls/InstanceCallsData.type';
+import { logger } from '../../misc/Logger';
 
 export class InstanceCallsService {
   private static redisClient = RedisClient.getInstance().client;
 
   static async getInstanceCalls(instanceId: string): Promise<number> {
+    logger.info(`Getting calls quantity for instance ${instanceId}`);
     const callsQuantity = await this.redisClient.zScore(config.redis.instanceSet, instanceId);
     if (callsQuantity === null) {
       await this.setInstanceCalls(instanceId, 0);
@@ -14,6 +16,7 @@ export class InstanceCallsService {
   }
 
   static async getAllInstancesSortedByCalls(): Promise<InstanceCallsData[]> {
+    logger.info(`Getting all instances sorted by calls quantity`);
     const instances = await this.redisClient.zRangeWithScores(config.redis.instanceSet, 0, -1, {});
     return instances.map(instance => ({
       instanceId: instance.value,
@@ -22,6 +25,7 @@ export class InstanceCallsService {
   }
 
   static async setInstanceCalls(instanceId: string, callsQuantity: number): Promise<void> {
+    logger.info(`Setting calls quantity for instance ${instanceId} to ${callsQuantity}`);
     await this.redisClient.zAdd(config.redis.instanceSet, {
       score: callsQuantity,
       value: instanceId,
@@ -29,24 +33,30 @@ export class InstanceCallsService {
   }
 
   static async incrementInstanceCalls(instanceId: string): Promise<void> {
+    logger.info(`Incrementing calls quantity for instance ${instanceId}`);
     await this.redisClient.zIncrBy(config.redis.instanceSet, 1, instanceId);
   }
 
   static async decrementInstanceCalls(instanceId: string): Promise<void> {
+    logger.info(`Decrementing calls quantity for instance ${instanceId}`);
     const currentCallsQuantity = await this.getInstanceCalls(instanceId);
     switch (true) {
       case currentCallsQuantity === 0:
+        logger.info(`Calls quantity for instance ${instanceId} is already 0`);
         return;
       case currentCallsQuantity < 0:
+        logger.info(`Calls quantity for instance ${instanceId} is negative, setting to 0`);
         await this.setInstanceCalls(instanceId, 0);
         return;
       default:
+        logger.info(`Decrementing calls quantity for instance ${instanceId}`);
         await this.redisClient.zIncrBy(config.redis.instanceSet, -1, instanceId);
         return;
     }
   }
 
   static async deleteInstanceData(instanceId: string): Promise<void> {
+    logger.info(`Deleting instance ${instanceId} data`);
     await this.redisClient.zRem(config.redis.instanceSet, instanceId);
   }
 }
