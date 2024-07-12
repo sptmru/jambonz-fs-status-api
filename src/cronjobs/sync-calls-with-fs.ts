c60e87097ee87cc6543701939828c9735c00f494
Soslan Aldatov <soslanaldatov@gmail.com>
2024-07-12 04:05:58 +0400

    sync instances with k8s in cronjob as well

--- src/cronjobs/sync-calls-with-fs.ts
+++ src/cronjobs/sync-calls-with-fs.ts
@@ -1,3 +1,5 @@
+import { CoreV1Api, KubeConfig } from '@kubernetes/client-node';
+
 import { InstanceCallsService } from '../services/instance/InstanceCallsService';
 import { config } from '../infrastructure/config/config';
 import axios from 'axios';
@@ -6,6 +8,33 @@ import { logger } from '../misc/Logger';
 logger.info(config.redis.uri);
 
 void (async (): Promise<void> => {
+  const kc = new KubeConfig();
+  kc.loadFromDefault();
+
+  const k8sApi = kc.makeApiClient(CoreV1Api);
+  const namespace: string = 'jambonz';
+  const serviceName = 'feature-server';
+
+  try {
+    const endpoints = await k8sApi.readNamespacedEndpoints(serviceName, namespace);
+    if (endpoints.body?.subsets !== undefined) {
+      if (endpoints.body.subsets.length > 0) {
+        const addresses = endpoints.body.subsets[0]?.addresses;
+        if (addresses && addresses.length > 0) {
+          for (const endpointData of addresses) {
+            const ipAddr = endpointData.ip;
+            const instance = await InstanceCallsService.getInstanceCalls(ipAddr);
+            if (!instance) {
+              await InstanceCallsService.setInstanceCalls(ipAddr, 0);
+            }
+          }
+        }
+      }
+    }
+  } catch (err) {
+    logger.error(`sync-calls-with-fs: error fetching endpoints from k8s: ${err}`);
+  }
+
   const instances = await InstanceCallsService.getAllInstancesSortedByCalls();
 
   for (const instance of instances) {
